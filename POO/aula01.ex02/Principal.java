c09f04843221319c1060af43f238dd6f020b9062
Milena Soares <mi_soaress2@hotmail.com>
2026-02-23 14:30:17 -0300

    Usando variável String msg para limpar as condicionais

---
+++ POO/aula01.ex02/Principal.java
@@ -0,0 +1,36 @@
+package empresa;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int valorCorreto = 10000;
+		Scanner teclado = new Scanner(System.in);
+		int palpite;
+		String msg;
+		System.out.println("Digite seu palpite: ");
+		palpite = teclado.nextInt();
+		
+		while(palpite!=valorCorreto) {
+			
+			msg = palpite>valorCorreto?"Um pouco menos...":"Um pouco mais...";
+			System.out.println(msg);
+			/*if(palpite>valorCorreto) {
+				System.out.println("Um pouco menos...");
+			}
+			else {
+				System.out.println("Um pouco mais...");
+				
+			}*/
+			
+			System.out.println("Digite outro palpite: ");
+			palpite = teclado.nextInt();
+		}
+		
+		System.out.println("Você acertou!");
+		
+
+	}
+
+}
